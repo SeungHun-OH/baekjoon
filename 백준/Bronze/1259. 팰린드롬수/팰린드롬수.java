72dab7a022f0234852d10fc2773f5f915f842381
SeungHun-OH <52911726+SeungHun-OH@users.noreply.github.com>
2025-08-28 12:50:55 +0900

    [Bronze I] Title: 팰린드롬수, Time: 64 ms, Memory: 11424 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(br.readLine());
+			if (sb.toString().equals("0")) break;
+			String eqStr = sb.toString();
+			if (eqStr.equals(sb.reverse().toString())) {
+				System.out.println("yes");
+			} else {
+				System.out.println("no");
+			}
+		}
+	}
+}
